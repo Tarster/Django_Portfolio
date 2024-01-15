d361d963ca2f53d343c5d5535d97b04e623d9438
Tarster <sharmatarun537@gmail.com>
2024-01-15 20:52:22 +0000

    adding models and finishing setup.

--- personal_website/blogs/admin.py
+++ personal_website/blogs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Author, Post, Tag
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('caption',)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('author_name', 'email')
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
